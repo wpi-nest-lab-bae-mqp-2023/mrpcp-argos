64ceb26bd9e9b5a86e4d4b693b3f54f00f227e25
Yaşar İdikut <yasar8000@gmail.com>
2024-04-09 13:22:17 -0400

    visualize current and goal positions for each robot from the loop function

--- loop_functions/kheperaiv_orca_loop/id_qtuser_functions.cpp
+++ loop_functions/kheperaiv_orca_loop/id_qtuser_functions.cpp
@@ -13,21 +13,6 @@ CIDQTUserFunctions::CIDQTUserFunctions() :
 
 void CIDQTUserFunctions::DrawID(CKheperaIVEntity& c_entity) {
    DrawText(CVector3(0.0, 0.0, 0.2), c_entity.GetId());
-//    auto &cController = dynamic_cast<CKheperaIVORCA &>(c_entity.GetControllableEntity().GetController());
-//
-//    if (cController.id != 0) { return; }
-//    CVector2 vel_vec = cController.curr_vel / 5.;
-//    DrawRay(CRay3(CVector3(0., 0., 0.1),
-//              CVector3(vel_vec.GetX(), vel_vec.GetY(), 0.1)),
-//        CColor(0xFF, 0xA6, 0xFE, 250), 3);
-//
-//    for (int i = 0; i < cController.n_vel_vecs.size(); ++i) {
-////        std::cout << "i:" << i << std::endl;
-//        DrawRay(CRay3(CVector3(cController.n_vel_vecs[i].pos.GetX(), cController.n_vel_vecs[i].pos.GetY(), 0.1),
-//                      CVector3(cController.n_vel_vecs[i].pos.GetX() + cController.n_vel_vecs[i].vel.GetX() / 5., cController.n_vel_vecs[i].pos.GetY() + cController.n_vel_vecs[i].vel.GetY() / 5., 0.1)),
-//                CColor(0xFF, 0xA6, 0xFE, 250), 3);
-//    }
-
 }
 
 void CIDQTUserFunctions::DrawInWorld() {
